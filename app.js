9e8b969bd10fc34cfc522da7fe7156429b8c3fef
DAZE\User <igli.ylli1594@gmail.com>
2022-06-18 12:41:50 +0200

    checked for proper links

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const links = document.querySelector("a");
+const links = document.querySelectorAll("a");
 
-// links.forEach((link) => {
-//   if (link.href.includes("https")) {
-//     link.style.color = "yellow";
-//   } else {
-//     link.style.color = "red";
-//   }
-// });
-
-for (let i = 0; i < links.length; i++) {
-  if (links[i].href.includes("https")) {
-    links[i].style.color = "yellow";
+links.forEach((link) => {
+  if (link.href.includes("https")) {
+    link.style.color = "yellow";
   } else {
-    links[i].style.color = "red";
+    link.style.color = "red";
   }
-}
+});
+
+// for (let i = 0; i < links.length; i++) {
+//   if (links[i].href.includes("https")) {
+//     links[i].style.color = "yellow";
+//   } else {
+//     links[i].style.color = "red";
+//   }
+// }
